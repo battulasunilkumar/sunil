5ab17d4684c727ab0ed5b9978d215ea490360327
Sunilkumar Battula <Sunilkumar Battula@sunilkumar>
2020-08-06 19:28:42 +0530

    snfs

---
+++ sunilsubbu/src/test/java/com/actime/testtrack/Demo.java
@@ -0,0 +1,16 @@
+package com.actime.testtrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void runTest2()
+	{
+		Reporter.log("successfully running  7",true);
+		Reporter.log("successfully running 8",true);
+		Reporter.log("successfully running 9",true);
+	}
+
+}
